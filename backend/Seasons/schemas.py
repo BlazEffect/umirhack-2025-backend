bbd5accf02cc90ffda4e34e69d7d429c506e3a55
Anna_Shevtsova9775 <annasevcova01@gmail.com>
2025-11-12 02:12:10 +0300

    Добавлены

--- backend/Seasons/schemas.py
+++ backend/Seasons/schemas.py
@@ -1,21 +1,21 @@
 from pydantic import BaseModel
 from typing import Optional
-from datetime import date
+from datetime import datetime
 
 class SeasonCreate(BaseModel):
     name: str
-    date_start: date
-    date_end: date
+    date_start: datetime
+    date_end: datetime
 
 class SeasonUpdate(BaseModel):
     name: Optional[str] = None
-    date_start: Optional[date] = None
-    date_end: Optional[date] = None
+    date_start: Optional[datetime] = None
+    date_end: Optional[datetime] = None
 
 class SeasonOut(BaseModel):
     id: int
     name: str
-    date_start: date
-    date_end: date
+    date_start: datetime
+    date_end: datetime
     class Config:
         from_attributes = True
